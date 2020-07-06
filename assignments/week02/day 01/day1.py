5dac027c6d5082fc8726084d537b6ca3847908ab
alfred-richards-au9 <richardsbenjialfred@gmail.com>
2020-07-06 15:18:58 +0530

    week 2 day 1 assignment

---
+++ assignments/week02/day 01/day1.py
@@ -0,0 +1,30 @@
+#division of two numbers
+a=5
+b=6
+c=a/b
+print(c)
+
+#printing float result in to int 
+q=5
+w=6
+e=q//w
+print(e)
+
+#using percentage operator tp know the remainder and it is used to know the odd or even pof a number
+r=7
+t=3
+y=r%t
+print(y)
+
+#concatination of strings 
+a="alfred"
+b="richards"
+
+print(a + " "+b)
+
+
+
+
+
+
+
