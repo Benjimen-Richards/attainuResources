985128f0c0365dd2d4ced06d9f1c2248bbaa90f9
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2020-07-08 16:49:22 +0530

    Update day2 coding challenge.py

--- coding-challenges/week02/day2 coding challenge.py
+++ coding-challenges/week02/day2 coding challenge.py
@@ -9,21 +9,21 @@ s3=int(input("enter english subject marks of the student\n"))
 s4=int(input("enter social subject marks of the student\n"))
 s5=int(input("enter chemistry marks of the student\n"))
 
-if(s1 or s2 or s3 or s4 or s5) >100
-    print("enter valid marks")
-    break
+    
 
 #percentage of the student marks of all the subject
 
 p=(s1+s2+s3+s4+s5)/5
+while((s1 or s2 or s3 or s4 or s5) <=100):
+    if(p>=90):   
+        print("student grade w.r.t marks of the subject is A")
+    elif((p>=70) and (p<=90)):
+        print("student grade w..r.t marks of the subject is B")
+    elif((p>=50)and (p<=70)):
+        print("student grade w.r.t. marks of the subject is C")
+    elif((p>=30) and (p<=50)):
+        print("student grade w,r.t. marks of the subjects is D")
+    elif(p<=30):
+        print("student grade w.r.t. marks of the subjects is E")
 
-if(p>=90):
-    print("student grade w.r.t marks of the subject is A")
-elif((p>=70) and (p<=90)):
-    print("student grade w..r.t marks of the subject is B")
-elif((p>=50)and (p<=70)):
-    print("student grade w.r.t. marks of the subject is C")
-elif((p>=30) and (p<=50)):
-    print("student grade w,r.t. marks of the subjects is D")
-elif(p<=30):
-    print("student grade w.r.t. marks of the subjects is E")
+print("enter valid marks")
