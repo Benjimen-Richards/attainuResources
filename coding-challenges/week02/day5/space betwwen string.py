2fa2f914ec16716accfb5b39ac2705655497fa83
alfred-richards-au9 <richardsbenjialfred@gmail.com>
2020-07-11 09:31:23 +0530

    cc for week2day5

---
+++ coding-challenges/week02/day5/space betwwen string.py
@@ -0,0 +1,8 @@
+#richards alfred
+if __name__ == "__main__":
+    a=input("enter a string\n")
+    b=a.isspace()
+    if b==True:
+        print("string contains space")
+    else:
+        print("no space between string")
