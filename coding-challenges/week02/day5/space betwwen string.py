b808ab759ab53632d582312fddc231cf5774d498
alfred-richards-au9 <richardsbenjialfred@gmail.com>
2020-07-13 22:45:19 +0530

    cc

--- coding-challenges/week02/day5/space betwwen string.py
+++
@@ -1,8 +0,0 @@
-#richards alfred
-if __name__ == "__main__":
-    a=input("enter a string\n")
-    b=a.isspace()
-    if b==True:
-        print("string contains space")
-    else:
-        print("no space between string")
---
+++ coding-challenges/week02/day5/spaces between given string.py
@@ -0,0 +1,7 @@
+#spaces between given string
+a="my name is alfred richards"
+count=0
+for i in a:
+   if(i.isspace()==True):
+      count+=1
+print(count)
