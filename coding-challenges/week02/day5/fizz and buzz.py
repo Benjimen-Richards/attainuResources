d495028ad2fa53cc6bed7154c3a8f6785d466ed2
alfred-richards-au9 <richardsbenjialfred@gmail.com>
2020-07-13 20:57:23 +0530

    week3day1 cc

--- coding-challenges/week02/day5/fizz and buzz.py
+++ coding-challenges/week02/day5/fizz and buzz.py
@@ -1,18 +1,13 @@
-def fizz():
-    for i in range(1,n,3):
-        print("fizz")
-        break
-def buzz():
-    for j in range(1,n,5):
-        print("buzz")
-        break
+
 if __name__ == "__main__":
     n=15
     for i in range(1,n+1):
-        if(i%3==0):
-            fizz()
+        if i%3==0 and i%5==0:
+             print("fizzbuzz ")
+        elif(i%3==0):
+            print("fizz")
         elif(i%5==0):
-            buzz()
+            print("buzz")
         else:
             print(i)
         i+=1
--- coding-challenges/week02/day5/reverse of a number.py
+++ coding-challenges/week02/day5/reverse of a number.py
@@ -1,9 +1,4 @@
-if __name__ == "__main__":
-    num=int(input("enter a number\n"))
-    r=0
-    while(num):
-        remainder=num%10
-        r=(r*10)+remainder
-        num=num//10
-    print("reverse of a number ",r)
- 
+a=input("enter a value")
+a=a[::-1]
+print(a)
+
