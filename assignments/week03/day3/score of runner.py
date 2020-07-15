36d6dae12c7b9084982538c99db4789b153bdb08
alfred-richards-au9 <richardsbenjialfred@gmail.com>
2020-07-15 21:47:36 +0530

    week3day3 assignment

---
+++ assignments/week03/day3/score of runner.py
@@ -0,0 +1,15 @@
+#Given the participants' score sheet for your University Sports Day, you are required t0
+# find the runner-up score. You are given scores. Store them in a list and find the score of
+#the runner-up.
+
+n=int(input("enter number of score present in score sheet\n"))
+scoresheet=[1,2,3,3,4,4,5]
+for i in range(1,n+1):
+    print("enter score of ",i)
+    x=int(input())
+    scoresheet.append(x)
+scoresheet.sort()
+b=list(dict.fromkeys(scoresheet))
+print("runnerup=",b[-2])
+for i in range(0,len(b)):
+    print(b[i],'',end="")
--- empty.py
+++
@@ -1,4 +0,0 @@
-from collections import Counter
-str='w3resource'
-b=dict(Counter(str))
-print(b)
