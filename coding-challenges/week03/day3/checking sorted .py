646c2f2567e12427a2a980606c9a7cdec10eaafd
alfred-richards-au9 <richardsbenjialfred@gmail.com>
2020-07-16 10:12:28 +0530

    week3day3

--- coding-challenges/week03/day3/checking sorted .py
+++ coding-challenges/week03/day3/checking sorted .py
@@ -1,19 +1,20 @@
 #checking string is sorted or not
 if __name__ == "__main__":
-   a=6
-   c=[]
-   for i in range(0,a):
-      b=int(input("enter ur number\n"))
-      c.append(b)
-   print(c)
-   flag=0
-   i=1
-   for i in range(1,len(c)):
-      if(c[i-1]<=c[i]):
-         flag=1
-      else:
-         flag=0
-   if(flag==1):
+   a=input("enter a string \n")
+   arr=[int(x) for x in a]
+   flag=""
+   for i in range(0,len(a)):
+      b=sorted(arr)
+      for i in range(0,len(arr)):
+         if(arr[i]==b[i]):
+               flag="0"
+         else:
+               flag="1"
+   if(flag=='0'):
+      print(b)
       print("sorted")
+      
    else:
-      print("not sorted")
+      print(arr)
+      print('not sorted')
+      
