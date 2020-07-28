2a6c69e0e939d69d10f2bd548df7488d7d8116e4
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2020-07-28 21:33:34 +0530

    week5day2

---
+++ coding-challenges/week05/day2/q2 mergesort.py
@@ -0,0 +1,40 @@
+#2. two sorted arrays.
+
+
+def merge(arr):
+    if len(arr)==1:
+        return 1
+    mid=len(arr)//2
+    l=arr[:mid]
+    r=arr[mid:]
+    merge(l)
+    merge(r)
+    i=j=k=0
+    while i<len(l) and j <len(r):
+        if l[i]<r[j]:
+            arr[k]=l[i]
+            i+=1
+        else:
+            arr[k]=r[j]
+            j+=1
+        k+=1
+    while i<len(l):
+        arr[k]=l[i]
+        i+=1
+        k+=1
+    while j<len(r):
+        arr[k]=r[j]
+        j+=1
+        k+=1
+    
+if __name__ == "__main__":
+    n=int(input("testcases\n"))
+    for i in range(n):
+        size=int(input('length of array'))
+        array=list(map(int,input().strip().split()))  
+        if size==len(array):
+            merge(array)
+            print(*array)
+        else:
+            print('size of array is more than give size\n')
+    
