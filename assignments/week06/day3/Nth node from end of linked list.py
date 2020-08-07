d0d8a62b0df97e2b9a853ac5ec946fd1ffbec467
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2020-08-07 19:18:33 +0530

    Update Nth node from end of linked list.py

--- assignments/week06/day3/Nth node from end of linked list.py
+++ assignments/week06/day3/Nth node from end of linked list.py
@@ -4,48 +4,42 @@ class node:
     def __init__(self,val):
         self.val=val
         self.next=None
-class linkedlist:
-    def add(self,head,x):
-        if head is None:
-            return node(x)
+
+def add(head,x):
+    if head is None:
+        return node(x)
+    cur=head
+    while cur.next!=None:
+        cur=cur.next
+    cur.next=node(x)
+    return head
+def nthnode(head,number):
+    cur=head
+    count=0
+    while cur!=None:
+        count+=1
+        cur=cur.next
+    l=(count)-number
+    if l>0:
         cur=head
-        while cur.next!=None:
+        while l>0:
             cur=cur.next
-        cur.next=node(x)
-        return head
-    def print(self,head):
-        cur=head
-        while cur!=None:
-            print(cur.val)
-            cur=cur.next
-    def nthnode(self,head,number):
-        cur=head
-        count=0
-        while cur!=None:
-            count+=1
-            cur=cur.next
-        l=(count)-number
-        if l>0:
-            cur=head
-            while l>0:
-                cur=cur.next
-                l-=1
-            res=cur
-            return res.val
-        elif l==0:
-            return head.val
-        else:
-            return -1
+            l-=1
+        res=cur
+        return res.val
+    elif l==0:
+        return head.val
+    else:
+        return -1
 
         
 if __name__ == "__main__":
     l,nth=map(int,input('enter length of list and nth node \n').strip().split())
     n=list(map(int,input(f"Enter list with {l} element \n").strip('][').split(',')))
     x=node(n[0])
-    y=linkedlist()
     if len(n)<=l:
         for i in range(1,l):
-            x=y.add(x,n[i])
-        print('nth node backwards is ',y.nthnode(x,nth))
+            x=add(x,n[i])
+        print('nth node backwards is ',nthnode(x,nth))
     else:
         print('list exceeds required size\n')
