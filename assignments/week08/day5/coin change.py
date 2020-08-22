d7bdbf1fc70423e6906a4c13fc7fa79a6d6c0db6
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2020-08-22 19:52:36 +0530

    week 8 day 5 assignment

---
+++ assignments/week08/day5/coin change.py
@@ -0,0 +1,13 @@
+def solve(coins,amount):
+    dp=[0]+[float('inf')]*(amount+1)
+    for coin in coins:
+        for val in range(amount+1):
+            dp[val]=min(dp[val],dp[val-coin]+1)
+    if dp[amount]!=float('inf'):
+        return dp[amount]
+    else:
+        return[-1] 
+
+
+if __name__ == "__main__":
+    print(solve([1,2,5],11))
---
+++ assignments/week08/day5/house robber.py
@@ -0,0 +1,9 @@
+def total(nums):
+    c,m=0,0
+    for i in nums:
+        print(f'1st c={c},m={m},i={i}')
+         c,m=m,max(c+i,m)
+    return m
+
+if __name__ == "__main__":
+    print(total([1,2,3,1]))
