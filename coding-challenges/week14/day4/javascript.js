8afb6bdcb780214d3a543df20dc4caa348d0ba64
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2020-10-05 20:52:50 +0530

    week 14 day 4 cc

--- coding-challenges/week14/day4/javascript.js
+++ coding-challenges/week14/day4/javascript.js
@@ -2,11 +2,10 @@ var a=8
 var b=9
 function add(a,b){
   if (a>b){
-    return a%5
+    console.log(a%5)
   }
   else{
-    return b%5
+    console.log(b%5)
   }
 }
-var c=add(a,b)
-console.log(c)
+add(a,b)
