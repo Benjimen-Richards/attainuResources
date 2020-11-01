b9a11ef0849c9ed6022a584affc379435be24801
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2020-11-01 11:48:58 +0530

    month test 4

--- monthly-tests/delivery app.js
+++ monthly-tests/delivery app.js
@@ -1,126 +1,23 @@
-<!doctype html>
-<html lang="en">
+$(document).ready(function(){
+  var cities = "https://raw.githubusercontent.com/nshntarora/Indian-Cities-JSON/master/cities.json";
 
-<head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <!-- Css file -->
-  <link href="./delivery app.css" rel="stylesheet" />
-  <!-- Bootstrap CSS -->
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
-    integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
-  <title>Chomp-Champ</title>
-  <style>
-    a{
-      text-decoration: none;
-    }
-  </style>
-</head>
+  $('#search-query').keyup(function () {
+      var searchQuery = $('#search-query').val();
+      var expression = new RegExp(searchQuery,"i");
+      $('#search-results').html('')
 
-<body>
-  <div class=".container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <a class="navbar-brand" href="#">Chomp-Champ</a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-  
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item active">
-            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item dropdown">
-            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
-              aria-haspopup="true" aria-expanded="false">
-              Dropdown
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Hyderabad</a>
-              <a class="dropdown-item" href="#">Bengaluru</a>
-              <a class="dropdown-item" href="#">Mumbai</a>
-              <a class="dropdown-item" href="#">Chennai</a>
-              <a class="dropdown-item" href="#">Kolkata</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">New Cities adding soon.. </a>
-            </div>
-          </li>
-  
-        </ul>
-        <form class="form-inline my-2 my-lg-0">
-          <input class="form-control mr-sm-2" style="border-radius: 10px;width: 300px;" type="search"
-            placeholder="Enter your search......" aria-label="Search">
-          <button class="btn btn-outline-danger my-2 my-sm-0" type="submit">Search</button>
-        </form>
-        <div class="mx-2">
-          <button class="btn btn-danger">Login</button>
-          <button class="btn btn-danger">Signup</button>
-  
-        </div>
-      </div>
-    </nav>
-    <footer>
-      <!-- 4 columns
-    - Company (links) - link to About Us page
-    - Careers (links)
-    - Social (icons)
-    - Contact (links) -->
-      <div class="About">
-        <!-- Footer first Row -->
-        <div class="Google_play">
-          <a href="#"><img style="width: 60px;,;height: 50px;" id='google_play' src="https://previews.123rf.com/images/fokaspokas/fokaspokas1804/fokaspokas180400100/98514263-mobile-phone-icon-white-icon-on-black-background-inversion-vector-illustration-.jpg"/ ></a>
-        </div>
-        <div class="header_items"><a style="text-decoration: none;" href="#">Company</a></div>
-        <div class="header_items"><a style="text-decoration: none;"  href="#">Careers</a></div>
-        <div class="header_items"><a style="text-decoration: none;"  href="#">Careers</a></div>
-        <div class="header_items"><a style="text-decoration: none;" href="#">Social</a></div>
-        <div class="header_items"><a style="text-decoration: none;" href="#">Contact</a></div>
-        <div class="app_store">
-          <div class="Google_play">
-            <a href="#"><img id='google_play' src="https://play.google.com/intl/en_us/badges/static/images/badges/en_badge_web_generic.png"/ ></a>
-          </div> 
-        </div>
-        
-      </div>
-      <div class="copyright">
-        <div class="copyright_items">
-          <a href="#"><img style="width: 100px;,height: 100px;" src="https://lh3.googleusercontent.com/SWJdrRPR-IYSklT7zqAFwH9pJWi5w4ebB7gmVgPCUiSuzJJN1nW2wGiPcFizfhv7oAlayVM=s85"/></a>
-        </div>
-        <div class="copyright_items">
-          <p id = 'copyRightText'
-          ;">&forall;2020Chomp-Champ</p>
-        </div>
-        
-        <div class="copyright_items" id="social_img">
-          <div id="social">
-            <a href="#"><img style="width: 40px;,height: 40px;" src="https://i.pinimg.com/originals/ef/b5/d9/efb5d90eeb07bb5914f094007da4c2f7.png"/></a>
-          </div>
-          <div id="social">
-            <a href="#"><img style="width: 40px;,height: 40px;"src="https://www.designbust.com/download/171/png/instagram_logo_color_icon512.png"/></a>
-          </div>
-          <div id="social">
-            <a class="social" href="#"><img style="width: 40px;,height: 40px;" src="https://cdn.iconscout.com/icon/free/png-256/twitter-213-569318.png"/></a>
-          </div>
-          
-         
-        </div>
-      </div>
-    </footer>
+      $.ajax({url:cities,
+              success:function(data){
+                  data = JSON.parse(data);
 
-    
-  </div>
-</body>
-<!-- Optional JavaScript; choose one of the two! -->
 
-<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
-  integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
-  integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
-<script src="./delivery app.js" />
-</html>
+                  if(data.length > 0) {
+                      $.each(data,function(err,value) {
+                          if (value.state.search(expression) === 0){
+                              $('#search-results').append('<li class="list-group-item link-class">'+value.name+'</li>');
+                          }
+                      })
+
+                  }
+              }})
+  })
