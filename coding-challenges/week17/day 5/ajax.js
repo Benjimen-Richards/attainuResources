65a19860ce80a160d918eebce49fc6680526ce01
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2020-11-03 15:52:47 +0530

    week 17 day 5 cc

--- coding-challenges/week17/day 5/ajax.js
+++ coding-challenges/week17/day 5/ajax.js
@@ -1,11 +1,41 @@
-$.ajax({
-    url: 'https://api.github.com/users',
-    type: 'get',
-    success: function(data){
-      for(let i=1;i<data.length;i++)
-      {
-        var li = '<li>'+data[i].login+'</li>'
-        $('#github').append(li)
-      }
-    }
+$(document).ready(function(){
+    var cities = "https://raw.githubusercontent.com/nshntarora/Indian-Cities-JSON/master/cities.json";
+   
+
+
+    $('#search-query').keyup(function()
+    {
+      var search = $('#search-query').val();
+      var expression = new RegExp(search,"i");
+     
+
+      $.ajax({
+          url: cities,
+          success:function(data)
+          {
+            data = JSON.parse(data)
+            if(data.length>0)
+            {
+              $.each(data , function(err,val)
+              {
+                if(val.state.search(expression)===0)
+                {
+                  $('#search-results').append('<li class="list-group-item link-class">'+val.name+'</li>');
+                  if ($('#search-results').val ()== ""){
+                    $('#search-result').html('')
+                  }
+                }
+              })
+
+            }
+            
+          }
+        // ajax
+      })
+      
+      // keyup func 
+    
+    })
+
+
 })
