2c7bce7608f67901d745393839e0950de7a2f5ea
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2020-11-04 09:41:07 +0530

    week 13 day 3 assignment

---
+++ assignments/week18/day 3/todo.js
@@ -0,0 +1,18 @@
+function postmodel()
+{
+    $(document).ready(function()
+    {
+        var todo = 'https://jsonplaceholder.typicode.com/todos'
+        $.ajax
+        ({
+            url : todo,
+            success : function(data)
+            {
+                for ( i = 0 ; i < data.length ; i++)
+                {
+                    $('div ul').append('ID','<li style="text-decoration: none;font-weight: bolder;" >'+data[i].id+'</li>','Title','<li style="text-decoration: none;font-weight: bolder;" >'+data[i].title+'</li>','<hr>')
+                }
+            }
+        })
+    })
+}
