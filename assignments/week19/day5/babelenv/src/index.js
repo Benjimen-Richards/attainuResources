33017882dbfe2a78f86d08e49a5adc917ea0a86f
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2020-11-06 21:57:34 +0530

    week 19 day 5 assignment

---
+++ assignments/week19/day5/babelenv/src/index.js
@@ -0,0 +1,7 @@
+import 'babel-polyfill';
+
+import user from './usermodel';
+import constants from './constants';
+
+const userprofile = new user()
+ console.log(userprofile)
---
+++ assignments/week19/day5/babelenv/src/usermodel.js
@@ -0,0 +1,17 @@
+class usermodel
+{
+    constructor()
+    {
+        this.name = ''
+    }
+    getname()
+    {
+        return this.name
+    }
+    setname(name)
+    {
+        this.name = name
+    }
+}
+
+export default usermodel;
