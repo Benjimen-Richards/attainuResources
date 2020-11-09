21827e0e92c0b71cb5e5b8863997bbd1a4701a9d
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2020-11-09 14:48:46 +0530

    Update ladderlength.py

--- coding-challenges/week18/day 5/ladderlength.py
+++ coding-challenges/week18/day 5/ladderlength.py
@@ -5,21 +5,23 @@ def solve(wordlist,beginWord,endWord):
     level = 1
     queue = deque([[beginWord,level]])
     while queue:
-        currword , currlevel = queue.popleft() , 1
-        if currlevel == endWord:
+        currword ,currlevel = queue.popleft()
+        if currword == endWord:
             return currlevel
-        for i in range(len(wordList)):
+        for i in range(len(currword)):
             for char in 'abcdefghijklmnopqrstuvwxyz':
-                nextword = currword[:i] + char + currword[i+1:]
-
+                nextword = currword[:i] + str(char) + currword[i+1:]
                 if nextword in wordList:
                     wordList.remove(nextword)
                     queue.append([nextword,currlevel+1])
+    return len(queue)        
+        
+
 
 if __name__ == "__main__":
-    beginWord = "hit",
-    endWord = "cog",
+    beginWord = "hit"
+    endWord = "cog"
     wordList = ["hot", "dot", "dog", "lot", "log", "cog"]
-    solve(wordList,beginWord,endWord)
+    print(solve(wordList,beginWord,endWord))
 
 
