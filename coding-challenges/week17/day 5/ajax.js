d16d75f06b221843eaebbd26192ced1161b845ce
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2020-11-10 20:50:28 +0530

    .

--- coding-challenges/week17/day 5/ajax.js
+++ coding-challenges/week17/day 5/ajax.js
@@ -1,40 +1,33 @@
-$(document).ready(function(){
-    var cities = "https://raw.githubusercontent.com/nshntarora/Indian-Cities-JSON/master/cities.json";
-    $('#search-results').innerText=''
+$(document).ready(function () {
+  var cities = "https://raw.githubusercontent.com/nshntarora/Indian-Cities-JSON/master/cities.json";
+  $('#search-results').innerText = ''
 
 
-    $('#search-query').keyup(function()
-    {
-      var search = $('#search-query').val();
-      var expression = new RegExp(search,"i");
-      
-
-      $.ajax({
-          url: cities,
-          success:function(data)
-          {
-            data = JSON.parse(data)
-            if(data.length>0)
-            {
-              $.each(data , function(err,val)
-              {
-                if(val.state.search(expression)===0)
-                {
-                  $('#search-results').append('<li class="list-group-item link-class">'+val.name+'</li>');
-          
-                }
-              })
+  $('#search-query').keyup(function () {
+    var search = $('#search-query').val();
+    var expression = new RegExp(search, "i");
+    $.ajax({
+      url: cities,
+      success: function (data) {
+        data = JSON.parse(data)
+        if (data.length > 0) {
+          $.each(data, function (err, val) {
+            if (val.state.search(expression) === 0) {
+              $('#search-results').append('<li class="list-group-item link-class">' + val.name + '</li>');
 
             }
-           
-            
-          }
-        // ajax
-      })
-      
-      // keyup func 
-    
+          })
+
+        }
+
+
+      }
+      // ajax
     })
+
+    // keyup func 
+
+  })
 
 
 })
