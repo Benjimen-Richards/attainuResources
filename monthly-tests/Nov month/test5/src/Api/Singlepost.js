b7cb49b8c7931d93d0c7a4a6ee9f2a92671ce5bf
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2020-11-28 18:45:34 +0530

    nov month

--- monthly-tests/Nov month/test5/src/Api/Singlepost.js
+++ monthly-tests/Nov month/test5/src/Api/Singlepost.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-class Post extends React.Component {
+class Singlepost extends React.Component {
   state = { data: null };
   componentDidMount() {
     fetch(
@@ -16,11 +16,15 @@ class Post extends React.Component {
     console.log(this.props);
     return (
       <ul>
+        {this.state.data &&
+          this.state.data.map((user) => {
+            return <h1 key={user.id}>{user.employee}</h1>;
+          })}
         <div>
-          <h1>Empolyee Here </h1>
+          <h1>Employee here</h1>
         </div>
       </ul>
     );
   }
 }
-export default Post;
+export default Singlepost;
