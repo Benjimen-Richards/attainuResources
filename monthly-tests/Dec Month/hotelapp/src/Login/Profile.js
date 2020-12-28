95fd7df4b4bc3ee1dc1f445bf7da2289890526cb
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2020-12-28 12:06:04 +0530

    update

--- monthly-tests/Dec Month/hotelapp/src/Login/Profile.js
+++ monthly-tests/Dec Month/hotelapp/src/Login/Profile.js
@@ -1,30 +1,30 @@
-import React,{Component} from 'react';
+import React, { Component } from 'react';
 
 const url = "http://localhost:5000/api/auth/userinfo";
 
-class Profile extends Component{
-    constructor(){
+class Profile extends Component {
+    constructor() {
         super()
 
-        this.state={
-            user:'',
-            error:''
+        this.state = {
+            user: '',
+            error: ''
         }
     }
 
-    handleLogout=() => {
+    handleLogout = () => {
         sessionStorage.removeItem('logintoken');
         sessionStorage.removeItem('roletoken');
         this.props.history.push('/login')
     }
 
-    render(){
-        console.log("this.state.user",this.state.user)
-        if(sessionStorage.getItem('logintoken') == null){
+    render() {
+        console.log("this.state.user", this.state.user)
+        if (sessionStorage.getItem('logintoken') == null) {
             this.props.history.push('/login')
         }
-        sessionStorage.setItem('roletoken',this.state.user.role)
-        return(
+        sessionStorage.setItem('roletoken', this.state.user.role)
+        return (
             <div className="container">
                 <div className="panel panel-warning">
                     <div className="panel-heading">
@@ -33,11 +33,11 @@ class Profile extends Component{
                     </div>
                     <div className="panel-body">
                         <h1>Hi {this.state.user.name}</h1>
-                        <h2>Your Email Id is {this.state.user.email}</h2>
-                        <h2>Your Role is {this.state.user.role}</h2>
+                        <h2>Your Email Id is : {this.state.user.email}</h2>
+                        <h2>Your Role is : {this.state.user.role}</h2>
                     </div>
                     <button className="btn btn-danger"
-                    onClick={this.handleLogout}>
+                        onClick={this.handleLogout}>
                         Logout
                     </button>
                 </div>
@@ -45,22 +45,22 @@ class Profile extends Component{
         )
     }
 
-    componentDidMount(){
-        fetch(url,{
-            method:'GET',
-            headers:{
-                'x-access-token':sessionStorage.getItem('logintoken')
+    componentDidMount() {
+        fetch(url, {
+            method: 'GET',
+            headers: {
+                'x-access-token': sessionStorage.getItem('logintoken')
             }
         })
-        .then((res) => res.json())
-        .then((data) => {
-            this.setState({
-                user:data
+            .then((res) => res.json())
+            .then((data) => {
+                this.setState({
+                    user: data
+                })
             })
-        })
 
-        if(this.props){
-            this.setState({error:this.props.location.search.split('=')[1]})
+        if (this.props) {
+            this.setState({ error: this.props.location.search.split('=')[1] })
         }
     }
 }
