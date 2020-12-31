36f3cd4becf7a49fc40e3fee6cf34414cadaeea1
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2020-12-31 08:13:32 +0530

    week 27 day 1 assignment

--- assignments/week 27/day 1/dropdownmenu/src/dropdown.js
+++ assignments/week 27/day 1/dropdownmenu/src/dropdown.js
@@ -1,17 +1,46 @@
-import React from 'react'
-const url = 'http://localhost:1234'
-class Dropdown extends React.component {
+import axios from 'axios'
+import React, { Component } from 'react'
+const url = 'http://localhost:1234/data'
+class Dropdown extends Component {
+    constructor() {
+        super()
+        this.state = {
+            data: '',
+            input: ''
+        }
+    }
+    renderselect = (data) => {
+        if (data) {
+            return (
+                data.map(item => (
+                    <option>{item.moviename}</option>))
+            )
+        }
+    }
+    handler = (e) => {
+        console.log(e.target.name, e.target.value)
+        this.setState({ [e.target.name]: e.target.value })
+    }
+    changehandler = () => {
+        // let data = JSON.stringify(this.state.input)
+        axios.post(url, ...this.state.input)
+    }
     render() {
+        console.log(this.state.input)
         return (
             <div>
                 <select>
                     <option>---select--</option>
+                    {this.renderselect(this.state.data)}
                 </select>
+                <input name='input' value={this.state.input} onChange={this.handler} />
+                <button onClick={this.changehandler}>Addtolist</button>
             </div>
         )
     }
     componentDidMount() {
-        .get(url)
+        axios.get(url).then(res => this.setState({ data: res.data }))
+
     }
 }
 export default Dropdown
