6fe4a8d8cf4d6b470bd5f31350c438e5d8a1d43d
alfred-richards-au9 <67587493+alfred-richards-au9@users.noreply.github.com>
2021-01-04 22:20:35 +0530

    week28day01cc

---
+++ coding-challenges/week28/Day01/corptpattern.py
@@ -0,0 +1,21 @@
+def printTwoElements(arr, size):
+    for i in range(size):
+
+        
+        if arr[abs(arr[i])-1] > 0:
+            arr[abs(arr[i])-1] = -arr[abs(arr[i])-1]
+
+
+        else:
+            print("The repeating element is", abs(arr[i]))
+
+
+
+    for i in range(size):
+        if arr[i] > 0:
+            print("The Missing element is", i + 1)
+
+
+arr = [1, 2, 3, 5, 6, 5]
+n = len(arr)
+printTwoElements(arr, n)
